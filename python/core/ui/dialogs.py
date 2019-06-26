f5a4a7cca2d755e63c349f06bfe438d7f1187ad9
brent <brent@tylerart.com>
2019-06-25 23:13:07 -0700

    added shiboken as option

--- python/core/ui/dialogs.py
+++ python/core/ui/dialogs.py
@@ -8,10 +8,15 @@ This can be used as either a starter template, subclassed, or called.
 try:
     from PySide.QtGui import *
     from PySide.QtCore import *
+    from shiboken import wrapInstance
 except ImportError:
     from PySide2.QtGui import *
     from PySide2.QtCore import *
     from PySide2.QtWidgets import *
+    from shiboken2 import wrapInstance
+
+# No longer use shiboken
+import maya.OpenMayaUI as omui
 
 
 class UI(QDialog):
@@ -20,14 +25,24 @@ class UI(QDialog):
     """
 
     def __init__(self, name='UI'):
-        super(UI, self).__init__()
+        super(UI, self).__init__(wrapInstance(long(omui.MQtUtil.mainWindow()), QWidget))
 
         # Set window title, upper right icons
+        self.setWindowTitle(name)
+        self.setWindowFlags(self.windowFlags() | Qt.WindowSystemMenuHint | Qt.WindowMinMaxButtonsHint)
 
         # Run main create function
         self.create_ui()
+        self.show()
 
     def create_ui(self):
         """Method intended to be overwritten when subclassed"""
         pass
 
+
+def main():
+    UI()
+
+
+if __name__ == '__main__':
+    main()
